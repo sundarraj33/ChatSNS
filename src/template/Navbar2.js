0be367b446eb03088dc6e2752380adbaaab86fc8
sundarraj33 <sundarraj.m033@gmail.com>
2024-08-01 18:04:16 +0530

    Sample Projects

--- src/template/Navbar2.js
+++ src/template/Navbar2.js
@@ -2,7 +2,7 @@ const Navbar2 = ()=>{
     return(
         <>
             <div className="navbar2 h-5/6 border-rose-950 border-2 sm:border-green-300 md:border-red-600 lg:border-sky-600 xl:border-purple-700 " >
-                <h2>hello</h2>
+                <h2 className="p-4 ">hello</h2>
             </div>
         </>
     )
