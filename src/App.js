c92e6ae800651063caf7f3d57253e15b3c6f7de0
sundarraj33 <sundarraj.m033@gmail.com>
2024-08-03 17:55:24 +0530

    main taple finished

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './input.css';
 import './output.css';
 import Navbar1 from './template/Navbar1';
 import Main from './template/Main';
+import Footer from './template/Footer';
 function App() {
   return (
     <>    
     <Navbar1/>
     <Main />
+    <Footer />
     </>
   );
 }
---
+++ src/template/Footer.js
@@ -0,0 +1,26 @@
+const Footer = ()=>{
+    return(
+        <>
+        <footer>
+            <div className="fotter-side" style={{'backgroundColor':'#06202d','height':'300px'}}>
+                <div className="text-white container mx-auto flex flex-row justify-space"> 
+                    <div className="intro p-5">                        
+                        <p className="text-2xl">ChatSNS.com</p>
+                        <p>Chat SearchNSave is designed to assist with your web searched and provide recommendations, It does not store any personal data or compromise your privacy.Your browsing experience remains secure and confudential</p>
+                        <ul>
+                            <li>
+
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="location">
+                        <p>6, Vivekanthar Street , Dubai Mian Road,Dubai.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        </>
+    )
+}
+
+export default Footer;
