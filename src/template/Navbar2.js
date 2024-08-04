3a90d0b67d91c5b121a317b7d310c0d69a94fb63
sundarraj33 <sundarraj.m033@gmail.com>
2024-08-04 22:27:18 +0530

    Footware Finished

--- src/template/Navbar2.js
+++ src/template/Navbar2.js
@@ -1,8 +1,9 @@
 const Navbar2 = ()=>{
     return(
         <>
-            <div className="navbar2 h-full border-rose-950 border-2 sm:border-green-300 md:border-red-600 lg:border-sky-600 xl:border-purple-700 " >
-                <h2 className="p-4 ">hello</h2>
+            <div className="navbar2 h-full " >
+                {/* <img src='https://portswigger.net/cms/images/22/e6/4281-article-chatgpt_copy.jpg'></img> */}
+                <img src='https://www.contractworks.com/hubfs/Imported_Blog_Media/ai-technology-brain-background-digital-transformation-concept-1.jpg' classname='' style={{height:'400px',width:'100%'}}></img>
             </div>
             <div className=" h-24 flex flex-row justify-center" style={{backgroundColor:'rgb(6,30,41)'}}>
                 <p className="text-white p-4 text-center text-xl mx-2 my-4 xl:text-3xl">Latest Marketing Ideas & Hacks </p>
