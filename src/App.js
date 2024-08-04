366b875b6a165a7919afd6faa2eb56f6d89a6721
sundarraj33 <sundarraj.m033@gmail.com>
2024-08-04 22:17:17 +0530

    Footer page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './output.css';
 import Navbar1 from './template/Navbar1';
 import Main from './template/Main';
 import Footer from './template/Footer';
+
 function App() {
   return (
     <>    
--- src/template/Footer.js
+++ src/template/Footer.js
@@ -3,9 +3,9 @@ const Footer = ()=>{
         <>
         <footer>
             <div className="fotter-side" style={{'backgroundColor':'#06202d','height':'300px'}}>
-                <div className="text-white container flex flex-row justify-between"> 
-                    <div className="intro p-5 my-3 w-100 bg-green-300">   
-                        <div className="m-3 w-28 bg-red-300">
+                <div className="text-white container mx-auto flex flex-row justify-between"> 
+                    <div className="intro p-5 m-5 ">   
+                        <div className="m-5 ">
                             <p className="text-2xl">ChatSNS.com</p>
                             <p>Chat SearchNSave is designed to assist with your web searched and provide recommendations, It does not store any personal data or compromise your privacy.Your browsing experience remains secure and confudential</p>
                             <ul>
@@ -16,7 +16,7 @@ const Footer = ()=>{
                             </div>                     
                         
                     </div>
-                    <div className="location m-9">
+                    <div className="location m-5 p-5">
                         <p>6, Vivekanthar Street , Dubai Mian Road,Dubai.</p>
                     </div>
                 </div>
