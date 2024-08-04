f47f089865b5c496a86f5ec7f66ac8d2d08c3d02
sundarraj33 <sundarraj.m033@gmail.com>
2024-08-04 22:29:29 +0530

    Footware Finished

--- src/template/Navbar2.js
+++ src/template/Navbar2.js
@@ -2,7 +2,7 @@ const Navbar2 = ()=>{
     return(
         <>
             <div className="navbar2 h-full " >
-                {/* <img src='https://portswigger.net/cms/images/22/e6/4281-article-chatgpt_copy.jpg'></img> */}
+                
                 <img src='https://www.contractworks.com/hubfs/Imported_Blog_Media/ai-technology-brain-background-digital-transformation-concept-1.jpg' classname='' style={{height:'400px',width:'100%'}}></img>
             </div>
             <div className=" h-24 flex flex-row justify-center" style={{backgroundColor:'rgb(6,30,41)'}}>
