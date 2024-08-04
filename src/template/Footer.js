b3d3b7f701f3fba4b8f782516056b1c4551b04e8
sundarraj33 <sundarraj.m033@gmail.com>
2024-08-04 21:42:11 +0530

    Footer page

--- src/template/Footer.js
+++ src/template/Footer.js
@@ -3,17 +3,20 @@ const Footer = ()=>{
         <>
         <footer>
             <div className="fotter-side" style={{'backgroundColor':'#06202d','height':'300px'}}>
-                <div className="text-white container mx-auto flex flex-row justify-space"> 
-                    <div className="intro p-5">                        
-                        <p className="text-2xl">ChatSNS.com</p>
-                        <p>Chat SearchNSave is designed to assist with your web searched and provide recommendations, It does not store any personal data or compromise your privacy.Your browsing experience remains secure and confudential</p>
-                        <ul>
-                            <li>
+                <div className="text-white container flex flex-row justify-between"> 
+                    <div className="intro p-5 my-3 w-100 bg-green-300">   
+                        <div className="m-3 w-28 bg-red-300">
+                            <p className="text-2xl">ChatSNS.com</p>
+                            <p>Chat SearchNSave is designed to assist with your web searched and provide recommendations, It does not store any personal data or compromise your privacy.Your browsing experience remains secure and confudential</p>
+                            <ul>
+                                <li>
 
-                            </li>
-                        </ul>
+                                </li>
+                            </ul>
+                            </div>                     
+                        
                     </div>
-                    <div className="location">
+                    <div className="location m-9">
                         <p>6, Vivekanthar Street , Dubai Mian Road,Dubai.</p>
                     </div>
                 </div>
