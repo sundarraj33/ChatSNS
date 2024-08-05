6b783b30cd57eb1dfc4855fd2d5f70125d105263
sundar raj <64121348+sundarraj33@users.noreply.github.com>
2024-08-05 22:56:30 +0530

    Update Footer.js

--- src/template/Footer.js
+++ src/template/Footer.js
@@ -5,7 +5,7 @@ const Footer = ()=>{
             <div className="fotter-side" style={{'backgroundColor':'#06202d','height':'300px'}}>
                 <div className="text-white container mx-auto flex flex-row justify-between"> 
                     <div className="intro p-5 m-5 ">   
-                        <div className="m-5 ">
+                        <div className="m-5">
                             <p className="text-2xl">ChatSNS.com</p>
                             <p>Chat SearchNSave is designed to assist with your web searched and provide recommendations, It does not store any personal data or compromise your privacy.Your browsing experience remains secure and confudential</p>
                             <ul>
